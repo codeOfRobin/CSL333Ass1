e8f6eb321b37d260e21df2c2f2d6076936f8bb38
Robin Malhotra <malhotra.robin@yahoo.co.in>
2015-01-25 17:42:49 +0530

    Initial Commit

---
+++ CSL333Ass1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CSL333Ass1
+//
+//  Created by Robin Malhotra on 25/01/15.
+//  Copyright (c) 2015 Batman And Robin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
