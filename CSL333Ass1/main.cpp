bbf07e28f1fc0ce9452aa3a6d8a8f2ece417b02c
Robin Malhotra <malhotra.robin@yahoo.co.in>
2015-01-26 02:27:09 +0530

    done with inputs

--- CSL333Ass1/main.cpp
+++ CSL333Ass1/main.cpp
@@ -6,10 +6,95 @@
 //  Copyright (c) 2015 Batman And Robin. All rights reserved.
 //
 
+#include <fstream>
 #include <iostream>
+#include <vector>
+#include <unistd.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+FILE *pFile;
+float timeInMinutes;//time for program to finish
+vector<char> V;
+vector<string>inputStrings;
+vector<vector<int>> costMatrix;
+
+int sizeOfVocab,k,numberOfStrings,costOfInsertion;
+
+void readText()
+{
+    char * dir = getcwd(NULL, 0);
+    cout << "Current dir: " << dir << endl;
+    pFile = fopen ( "./input.txt" , "rb" );
+    if (pFile==NULL) {fputs ("File error",stderr); exit (1);}
+    
+    // obtain file size:
+    fseek (pFile , 0 , SEEK_END);
+    long lSize = ftell (pFile);
+    rewind (pFile);
+    
+    // allocate memory to contain the whole file:
+    char *buffer = (char*) malloc (sizeof(char)*lSize);
+    if (buffer == NULL) {fputs ("Memory error",stderr); exit (2);}
+    
+    // copy the file into the buffer:
+    size_t result = fread (buffer,1,lSize,pFile);
+    if (result != lSize) {fputs ("Reading error",stderr); exit (3);}
+    
+    /* the whole file is now loaded in the memory buffer. */
+    
+    
+    char * pch;
+    pch = strtok (buffer," \n,");
+    
+    timeInMinutes=atof(pch);
+    
+    pch = strtok (NULL," \n,");
+    sizeOfVocab=atoi(pch);
+    
+    pch = strtok (NULL," \n,");
+    for (int i=0; i<sizeOfVocab; i++)
+    {
+        V.push_back(*pch);
+        pch = strtok (NULL," \n,");
+    }
+    
+    
+    numberOfStrings=atoi(pch);
+    
+    for (int i=0; i<numberOfStrings;i++)
+    {
+        pch = strtok (NULL," \n,");
+        inputStrings.push_back(pch);
+    }
+    
+    
+    pch = strtok (NULL," \n,");
+    costOfInsertion=atof(pch);
+    
+    costMatrix.resize(sizeOfVocab+1);
+    pch = strtok (NULL," \n,");
+
+    for (int i=0; i<sizeOfVocab+1; i++)
+    {
+        vector<int> v(sizeOfVocab+1);
+        for (int j=0; j<sizeOfVocab+1; j++)
+        {
+            v[j]=atoi(pch);
+            pch = strtok (NULL," \n");
+        }
+        costMatrix[i]=v;
+    }
+    
+    
+
+    // terminate
+    fclose (pFile);
+}
+
+
+int main(int argc, const char * argv[])
+{
+   
+    readText();
     return 0;
 }
